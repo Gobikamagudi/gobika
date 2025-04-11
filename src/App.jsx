aa725062e51cc9f70c3842da3345c393146cd03e
Gobikamagudi <gobikamagudi19@gmail.com>
2025-04-11 16:37:21 +0530

    Its a vite-react application

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,41 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [text,setText]= useState("");
-  const [results, setResults] = useState([]);
-  console.log(results)
-  return <>
-  <h1>WELCOME EVERYONE! </h1>
-  <input type="text" placeholder="Enter to search" onChange={(e) => setText(e,EventTarget.value)} />
-  <button 
-    onClick={() => {
-      fetch("/api/search?text=" + text)
-      .then((response) => {
-        response
-        .json()
-        .then((results) => setResults(results))
-        .catch((error) => console.error("Failed to search...", error));
-      })
-      .catch((error) => console.error("Failed to search...", error));
-    }}
-    >
-      Search
-    </button>
-    <ul>
-        {
-          results.map((item, index) => (
-            <li key={index}>
-              <a href={item.url} target="_blank">
-                {item.title}
-              </a>
-            </li>
-          ))
-          
-        }
-      </ul>
-      
-
-
-    
-  </>
+import {useState, usestate} from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GridView from './GridView'
+ import Sample from './Sample'
+import Profilecard from './Profilecard'
+import Home from "./Home";
+import NavBar from './NavBar'
+import Todo from './Todo'
+import storeredux from './storeredux'
+import { Provider } from 'react-redux';
+const profileList = {
+  name: "Gobika",
+  Department: "AIDS",
+  year: 2,
+  mobile: 9834562748,
+  address: "D.No: 23/234,Saraswathi Nagar,Coimbatore",
 };
 
-export default App;
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        {/* <Route path="/Todo" element={<Todo />} /> */}
+        <Route path="/Profilecard" element={<Profilecard profile={profileList} />} />
+        <Route path="/GridView" element={<GridView />} />
+        <Route path="/reduxcounter" element={<Provider store={storeredux}>
+      <Todo/>
+    </Provider>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+
+  );
+}
+
+export default App; 
